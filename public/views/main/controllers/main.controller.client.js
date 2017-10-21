faa38d58bb33e922b0840348085722ff545d700a
beanrider <jweng2014@hotmail.com>
2017-10-21 10:55:28 -0400

    Merge branch 'speech-service'
    # Conflicts:
    #	.gitignore
    #	public/services/main.service.client.js
    #	public/views/main/controllers/main.controller.client.js
    #	public/views/main/templates/main.view.client.html
    #	server/services/google.api/voice-to-text/voice-to-text.service.server.js

--- public/views/main/controllers/main.controller.client.js
+++ public/views/main/controllers/main.controller.client.js
@@ -18,19 +18,7 @@
 
             let player = document.getElementById('player');
             let mr;
-            let handleSuccess = function(stream) {
-                // var context = new AudioContext();
-                // var source = context.createMediaStreamSource(stream);
-                // var processor = context.createScriptProcessor(1024, 1, 1);
-                //
-                // source.connect(processor);
-                // processor.connect(context.destination);
-                //
-                // processor.onaudioprocess = function(e) {
-                //     // Do something with the data, i.e Convert this to ogg
-                //     console.log(e.inputBuffer);
-                // };
-
+            let handleSuccess = function(stream) {=
                 console.log("handleSuccess");
                 mr = new MediaRecorder(stream, {mimeType: "audio/webm;codecs=opus"});
                 mr.ondataavailable = ondataavailable;
@@ -96,32 +84,12 @@
                 });
             };
 
-
-
-            //     = function () {
-            //     try {
-            //         mr.stop();
-            //         mr.stream.getTracks().forEach(function(track)       {track.stop();});
-            //     } catch (e) {}
-            //
-            //     let blob = new Blob(buffer, {
-            //         type: 'data:audio/ogg;base64'
-            //     });
-            //
-            //     let url = window.URL.createObjectURL(blob);
-            //     download.href = url;
-            //     download.download = "123";
-            //     download.click();
-            //     // window.URL.revokeObjectURL(url);
-            // };
-
-
             navigator.mediaDevices.getUserMedia({ audio: true, video: false })
                 .then(handleSuccess);
 
             var video = document.getElementById('video');
 
-// Get access to the camera!
+            // Get access to the camera!
             if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
                 // Not adding `{ audio: true }` since we only want video now
                 navigator.mediaDevices.getUserMedia({video: true},image_format= "jpeg",
@@ -134,7 +102,7 @@
             var context = canvas.getContext('2d');
             var video = document.getElementById('video');
 
-// Trigger photo take
+            // Trigger photo take
             document.getElementById("snap").addEventListener("click", function () {
                 context.drawImage(video, 0, 0, 640, 480);
                 var image = new Image();
@@ -159,7 +127,7 @@
 
         function output() {
             console.log("in output ");
-            promise = mainService.faceRecon();
+            let promise = mainService.faceRecon();
             promise.success(function (text) {
                 if (text) {
                     console.log("the text is " + text);
@@ -168,21 +136,10 @@
                     vm.error = 'text not found';
                 }
             });
-
         }
 
         function voiceToText() {
-            promise = mainService.voiceToText();
-            promise.success(function (text) {
-                if (text) {
-                    console.log("the text is " + text);
-                    vm.voiceText = text;
-                } else {
-                    vm.error = 'text not found';
-                }
-            });
-            promise = mainService.nlp();
-
+            let promise = mainService.nlp();
         }
     }
 })();
